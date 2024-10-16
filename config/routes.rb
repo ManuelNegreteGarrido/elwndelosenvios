0a23fe2bbd38acce9e043d3f5a2936a09d28ed8e
manuel <mnegretegarrido@icluod.com>
2024-10-16 14:23:49 +0000

    tercer commit: se agrega vinculo entre vistas de retiro

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :retiros
+  resources :retiros, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+
   get 'dashboard/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
